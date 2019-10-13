ae2605b10529b139816491175346632c59e41eee
ivan mujica <ifmm87@gmail.com>
2019-10-13 16:56:03 -0400

    binary-gap

--- iterations/binary-gap.js
+++ iterations/binary-gap.js
@@ -2,12 +2,14 @@ const solution = (n) => {
   n = (n).toString(2);
   let array = n.split('');
   let countZeros = 0;
+  let onesIndex = [];
   for(i = 0; i < array.length; i++) {
-    if (array[i] === 0) {
-      // iniciamos
-      countZeros ++;
-    }
+    if (array[i] === 1 )
+       onesIndex.push(i);
+    
   }
+  console.log('============================================');
+  console.log(`ìndex ${onesIndex}`);
   return n;
 }
 
