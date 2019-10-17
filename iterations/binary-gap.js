48a0f2ff646d68f99f048bf6e27032db60fe2061
Ivancho <ifmm87@gmail.com>
2019-10-16 23:28:40 -0400

    binarygap

--- iterations/binary-gap.js
+++ iterations/binary-gap.js
@@ -1,15 +1,29 @@
-const solution = (n) => {
-  n = (n).toString(2);
-  let array = n.split('');
-  let countZeros = 0;
-  for(i = 0; i < array.length; i++) {
-    if (array[i] === 0) {
-      // iniciamos
-      countZeros ++;
+function solution(N) {
+  // write your code in JavaScript (Node.js 8.9.4)
+  try {
+    if (typeof N !== 'number' || N < 0) throw 'Invalid Number';
+    N = N.toString(2);
+    let diff = 0;
+    let oneIndexs = [],
+      differences = [];
+    for (let i = 0; i < N.length; i++) {
+      if (N[i] === '1') oneIndexs.push(i);
     }
+    // console.log('=====================================data=======');
+    // console.log(oneIndexs, N);
+    if (oneIndexs.length === 1) return 0;
+    else if (oneIndexs.length === 2) return oneIndexs[1] - oneIndexs[0] - 1;
+    else if (oneIndexs.length > 2) {
+      for (let j = 0; j < oneIndexs.length - 1; j++) {
+        if (oneIndexs[j + 1] - oneIndexs[j] > diff) {
+          diff = oneIndexs[j + 1] - oneIndexs[j];
+        }
+      }
+    }
+    return diff - 1;
+  } catch (error) {
+    return 0;
   }
-  return n;
 }
 
-console.log('============================================');
-console.log(solution(2000));
+console.log(solution(1610612737));
